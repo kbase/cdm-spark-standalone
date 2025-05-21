df8ca769dfc3481b28e38e3fe0a56419ed7272e2
Tianhao-Gu <tgu@anl.gov>
2025-05-21 09:52:54 -0500

    handle SIGTERM signal

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,23 @@
 
 . /opt/scripts/setup.sh
 
+# Setup signal handling
+term_handler() {
+  echo "Received SIGTERM, shutting down gracefully..."
+  # Forward the signal to the child process (which will be the Spark process)
+  kill -TERM "$child" 2>/dev/null
+  wait "$child"
+  exit 0
+}
+
+# Register the handler for SIGTERM
+trap 'term_handler' SIGTERM
+
 # In bitnami/spark Dockerfile, the entrypoint is set to /opt/bitnami/scripts/spark/entrypoint.sh and followed
 # by CMD ["/opt/bitnami/scripts/spark/run.sh"] meaning that the entrypoint is expected the run.sh script as an argument.
 # reference: https://github.com/bitnami/containers/blob/main/bitnami/spark/3.5/debian-12/Dockerfile#L69
-exec /opt/bitnami/scripts/spark/entrypoint.sh "$@" /opt/bitnami/scripts/spark/run.sh
+/opt/bitnami/scripts/spark/entrypoint.sh "$@" /opt/bitnami/scripts/spark/run.sh &
+child=$!
+
+echo "Spark application started with PID: $child"
+wait "$child"
